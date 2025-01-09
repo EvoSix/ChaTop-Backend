571887931046a0199612858cced71f8eefb0ced7
EvoSix <49368909+EvoSix@users.noreply.github.com>
2025-01-09 19:20:26 +0100

    ok

--- chatop/src/main/java/fr/oc/chatop/entity/Rental.java
+++ chatop/src/main/java/fr/oc/chatop/entity/Rental.java
@@ -1,17 +1,14 @@
 package fr.oc.chatop.entity;
 
 import jakarta.persistence.*;
-<<<<<<< HEAD
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-=======
->>>>>>> parent of 035f8dd (Almost Finished)
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Table(name="rentals")
-
+@Getter
+@Setter
 public class Rental {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,96 +24,7 @@ public class Rental {
     @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
 
-   
     private String created_at;
-
     private String updated_at;
 
-    public Rental(Long id, String name, Long surface, Double price, String picture, String description, User owner, String created_at, String updated_at) {
-        this.id = id;
-        this.name = name;
-        this.surface = surface;
-        this.price = price;
-        this.picture = picture;
-        this.description = description;
-        this.owner = owner;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-    }
-
-    public Rental() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getSurface() {
-        return surface;
-    }
-
-    public void setSurface(Long surface) {
-        this.surface = surface;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getPicture() {
-        return picture;
-    }
-
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public User getOwner() {
-        return owner;
-    }
-
-    public void setOwner(User owner_id) {
-        this.owner = owner_id;
-    }
-
-    public String getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(String created_at) {
-        this.created_at = created_at;
-    }
-
-    public String getUpdated_at() {
-        return updated_at;
-    }
-
-    public void setUpdated_at(String updated_at) {
-        this.updated_at = updated_at;
-    }
 }
